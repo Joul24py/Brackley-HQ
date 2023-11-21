9b65ba72599132b37aa2310d93cc8a9b8aff638c
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2023-11-21 11:07:10 -0600

    First approach to Buddy System

--- Manager/Manager.cpp
+++ Manager/Manager.cpp
@@ -1,19 +1,144 @@
-// Manager.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <cmath>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class BuddyAllocator {
+public:
+    BuddyAllocator(int totalSize);
+    ~BuddyAllocator();
+
+    void* allocate(int size);
+    void deallocate(void* ptr);
+
+    void display();
+
+private:
+    struct Block {
+        int size;
+        bool free;
+    };
+
+    int totalSize;
+    int levels;
+    std::vector<std::vector<Block>> memory;
+
+    int findLevel(int size);
+    void split(int level, int index);
+    void merge(int level, int index);
+};
+
+BuddyAllocator::BuddyAllocator(int totalSize) : totalSize(totalSize) {
+    levels = static_cast<int>(std::log2(totalSize) + 1);
+    memory.resize(levels);
+
+    for (int i = 0; i < levels; ++i) {
+        int blockSize = totalSize / (1 << i);
+        int blockCount = totalSize / blockSize;
+
+        memory[i].resize(blockCount);
+        for (int j = 0; j < blockCount; ++j) {
+            memory[i][j].size = blockSize;
+            memory[i][j].free = true;
+        }
+    }
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+BuddyAllocator::~BuddyAllocator() {
+    // Deallocate all memory before destroying the allocator (not implemented in this example)
+    // Deallocate all memory before destroying the allocator
+    for (int i = 0; i < levels; ++i) {
+        for (int j = 0; j < memory[i].size(); ++j) {
+            // Solo desasignar bloques ocupados para evitar errores
+            if (!memory[i][j].free) {
+                deallocate(static_cast<void*>(&memory[i][j]));
+            }
+        }
+    }
+}
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+void* BuddyAllocator::allocate(int size) {
+    int level = findLevel(size);
+
+    for (int i = level; i < levels; ++i) {
+        for (int j = 0; j < memory[i].size(); ++j) {
+            if (memory[i][j].free) {
+                // Split blocks until the desired size is reached
+                while (memory[i][j].size > size) {
+                    split(i, j);
+                }
+
+                memory[i][j].free = false;
+                return static_cast<void*>(&memory[i][j]);
+            }
+        }
+    }
+
+    // Unable to allocate memory
+    return nullptr;
+}
+
+void BuddyAllocator::deallocate(void* ptr) {
+    Block* block = static_cast<Block*>(ptr);
+    int level = findLevel(block->size);
+    int index = static_cast<int>(block - &memory[level][0]);
+
+    memory[level][index].free = true;
+
+    // Merge blocks until reaching the top level or finding a non-free buddy
+    while (level > 0 && memory[level][index].free) {
+        int buddyIndex = (index % 2 == 0) ? (index + 1) : (index - 1);
+        if (!memory[level][buddyIndex].free) {
+            break;
+        }
+
+        merge(level, index);
+        index /= 2;
+        --level;
+    }
+}
+
+void BuddyAllocator::split(int level, int index) {
+    memory[level][index].size /= 2;
+    memory[level - 1][2 * index].size = memory[level][index].size;
+    memory[level - 1][2 * index + 1].size = memory[level][index].size;
+}
+
+void BuddyAllocator::merge(int level, int index) {
+    int buddyIndex = (index % 2 == 0) ? (index + 1) : (index - 1);
+    memory[level][index / 2].size *= 2;
+    memory[level][index / 2].free = true;
+}
+
+int BuddyAllocator::findLevel(int size) {
+    return levels - static_cast<int>(std::log2(totalSize / size));
+}
+
+void BuddyAllocator::display() {
+    for (int i = 0; i < levels; ++i) {
+        std::cout << "Level " << i << ": ";
+        for (int j = 0; j < memory[i].size(); ++j) {
+            std::cout << "[" << (memory[i][j].free ? 'F' : 'U') << "]";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    BuddyAllocator allocator(64);
+
+    // Allocate memory
+    void* block1 = allocator.allocate(8);
+    void* block2 = allocator.allocate(16);
+    void* block3 = allocator.allocate(32);
+
+    // Display the state of the memory after allocations
+    allocator.display();
+
+    // Deallocate memory
+    allocator.deallocate(block2);
+
+    // Display the state of the memory after deallocation
+    allocator.display();
+
+    return 0;
+}
